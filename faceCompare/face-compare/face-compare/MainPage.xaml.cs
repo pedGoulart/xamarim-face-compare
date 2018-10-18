b87722d10364c5cc386b51d76c49740f3b4e6896
Pedro Goulart <phgoulart@latam.stefanini.com>
2018-10-18 18:05:16 -0300

    Creating constructor and first constants

--- faceCompare/face-compare/face-compare/MainPage.xaml.cs
+++ faceCompare/face-compare/face-compare/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ProjectOxford.Face;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,21 @@ namespace face_compare
 {
     public partial class MainPage : ContentPage
     {
+        private readonly IFaceServiceClient _faceServiceClientDetect;
+        private readonly IFaceServiceClient _faceServiceClientVerify;
+
+        //Creting consts to keep mines uri saved
+        const string uriFaceDetect = "";
+        const string uriFaceVerify = "";
+        const string key = "";
+
         public MainPage()
         {
             InitializeComponent();
+
+            //Initialize connection with API
+            _faceServiceClientDetect = new FaceServiceClient(key, uriFaceDetect);
+            _faceServiceClientVerify = new FaceServiceClient(key, uriFaceVerify);
         }
     }
 }
